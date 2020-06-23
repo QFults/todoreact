41b9f453bc9527d5834405006e49a2e5f71dba03
Quinton Fults <coding.banjo@gmail.com>
2020-06-23 10:28:43 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [itemState, setItemState] = useState({
+    item: '',
+    items: []
+  })
+
+  itemState.handleInputChange = event => {
+    setItemState({ ...itemState, [event.target.name]: event.target.value })
+  }
+
+  itemState.handleAddItem = event => {
+    event.preventDefault()
+    let items = JSON.parse(JSON.stringify(itemState.items))
+    items.push({
+      text: itemState.item,
+      isDone: false
+    })
+    setItemState({ ...itemState, items, item: '' })
+  }
+  
   return (
-    <h1>Hello World!</h1>
+    <>
+      <form>
+        <p>
+          <label htmlFor="item">item</label>
+          <input
+            type="text"
+            name="item"
+            id="item"
+            value={itemState.item}
+            onChange={itemState.handleInputChange} />
+        </p>
+        <p>
+          <button onClick={itemState.handleAddItem}>Add Item</button>
+        </p>
+      </form>
+      <ul>
+        {
+          itemState.items.map(item => <li>{item.text}</li>)
+        }
+      </ul>
+    </>
   )
 }
 
